967f19e60eae67154f24724e1c1ca7424613036a
Ondrej Sika <ondrej@ondrejsika.com>
2024-11-25 02:16:40 +0100

    feat(examples/terraform/k3s_auto): Add lab ssh key MD5

--- examples/terraform/k3s_auto/main.tf
+++ examples/terraform/k3s_auto/main.tf
@@ -13,14 +13,22 @@ provider "digitalocean" {
   token = var.digitalocean_token
 }
 
+
+locals {
+  ondrejsika = "0b:e9:c2:df:2e:89:89:6f:92:dc:b7:60:83:20:21:c0"
+  lab        = "a4:3f:f9:bd:18:45:a2:e2:0c:94:94:09:a6:1a:dd:ef"
+  ssh_keys = [
+    local.ondrejsika,
+    local.lab,
+  ]
+}
+
 resource "digitalocean_droplet" "main" {
   image  = "debian-12-x64"
   name   = "k3s-auto"
   region = "fra1"
   size   = "s-2vcpu-2gb"
-  ssh_keys = [
-    "0b:e9:c2:df:2e:89:89:6f:92:dc:b7:60:83:20:21:c0",
-  ]
+  ssh_keys = local.ssh_keys
   user_data = <<EOF
 #cloud-config
 ssh_pwauth: yes
