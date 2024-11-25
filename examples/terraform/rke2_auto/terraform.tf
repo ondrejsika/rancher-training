f8b2a2a9349aac11d2444ee326282e573e5edadb
Ondrej Sika <ondrej@ondrejsika.com>
2024-11-25 02:15:43 +0100

    feat(examples/terraform/rke2_auto): Add lab ssh key MD5

--- examples/terraform/rke2_auto/terraform.tf
+++ examples/terraform/rke2_auto/terraform.tf
@@ -13,14 +13,21 @@ provider "digitalocean" {
   token = var.digitalocean_token
 }
 
-resource "digitalocean_droplet" "master0" {
-  image  = "debian-12-x64"
-  name   = "rke2-auto-ma-0"
-  region = "fra1"
-  size   = "s-2vcpu-2gb"
+locals {
+  ondrejsika = "0b:e9:c2:df:2e:89:89:6f:92:dc:b7:60:83:20:21:c0"
+  lab        = "a4:3f:f9:bd:18:45:a2:e2:0c:94:94:09:a6:1a:dd:ef"
   ssh_keys = [
-    "0b:e9:c2:df:2e:89:89:6f:92:dc:b7:60:83:20:21:c0",
+    local.ondrejsika,
+    local.lab,
   ]
+}
+
+resource "digitalocean_droplet" "master0" {
+  image     = "debian-12-x64"
+  name      = "rke2-auto-ma-0"
+  region    = "fra1"
+  size      = "s-2vcpu-2gb"
+  ssh_keys  = local.ssh_keys
   tags      = ["rke2-auto", "rke2-auto-ma"]
   user_data = <<EOF
 #cloud-config
@@ -51,13 +58,11 @@ EOF
 resource "digitalocean_droplet" "master" {
   count = 2
 
-  image  = "debian-12-x64"
-  name   = "rke2-auto-ma-${count.index + 1}"
-  region = "fra1"
-  size   = "s-2vcpu-2gb"
-  ssh_keys = [
-    "0b:e9:c2:df:2e:89:89:6f:92:dc:b7:60:83:20:21:c0",
-  ]
+  image     = "debian-12-x64"
+  name      = "rke2-auto-ma-${count.index + 1}"
+  region    = "fra1"
+  size      = "s-2vcpu-2gb"
+  ssh_keys  = local.ssh_keys
   tags      = ["rke2-auto", "rke2-auto-ma"]
   user_data = <<EOF
 #cloud-config
@@ -91,13 +96,11 @@ EOF
 resource "digitalocean_droplet" "worker" {
   count = 2
 
-  image  = "debian-12-x64"
-  name   = "rke2-auto-wo-${count.index}"
-  region = "fra1"
-  size   = "s-2vcpu-2gb"
-  ssh_keys = [
-    "0b:e9:c2:df:2e:89:89:6f:92:dc:b7:60:83:20:21:c0",
-  ]
+  image     = "debian-12-x64"
+  name      = "rke2-auto-wo-${count.index}"
+  region    = "fra1"
+  size      = "s-2vcpu-2gb"
+  ssh_keys  = local.ssh_keys
   tags      = ["rke2-auto", "rke2-auto-wo"]
   user_data = <<EOF
 #cloud-config
